1477b97e81f8e6a3fc2ad65f4fe64e7760a18564
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-03-09 20:45:40 +0100

    fix Project.toml, add basic test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,29 @@
 using KernelOps
 using Test
+
+@inline function op_mean(A,r,c)
+    m=zero(eltype(A))
+    @inbounds begin
+        @simd for i in r
+            m+=A[i]
+        end
+    end
+    m/length(r)
+end
+@inline function op_max(A,r,c)
+    m=A[r[1]]
+    @inbounds begin
+        @simd for i in r
+            m=max(m,A[i])
+        end
+    end
+    m
+end
+
+@testset "Basic Test" begin
+    M=[exp(-(x-3)^2-(y-3)^2) for x in 1:5, y in 1:5]
+    f1=KernelOp(op_mean,M,(1,1))
+    f2=KernelOp(op_max,f1,(1,1))
+
+    @test (f1.==f2) == Bool[0 0 0 0 0; 0 0 0 0 0; 0 0 1 0 0; 0 0 0 0 0; 0 0 0 0 0]
+end
