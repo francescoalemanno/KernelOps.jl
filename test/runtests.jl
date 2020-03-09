43b1c0c638e7c1df77c5b8201f9329ac98ce572c
Francesco Alemanno <francescoalemanno710@gmail.com>
2020-03-09 21:03:23 +0100

    comment code in test

--- test/runtests.jl
+++ test/runtests.jl
@@ -28,14 +28,25 @@ end
     @test (f1.==f2) == Bool[0 0 0 0 0; 0 0 0 0 0; 0 0 1 0 0; 0 0 0 0 0; 0 0 0 0 0]
 end
 
-using KernelOps
-
-M=[exp(-(x-3)^2-(y-3)^2) for x in 1:5, y in 1:5]
-
-f1=KernelOp(M,(1,1)) do A,Is,I
-    length(Is) != 9 && return zero(eltype(A)) 
-    A[I]
-end
-f2=KernelOp(op_max,f1,(1,1))
-
-@test (f1.==f2) == Bool[0 0 0 0 0; 0 0 0 0 0; 0 0 1 0 0; 0 0 0 0 0; 0 0 0 0 0]
+# using KernelOps
+#
+# M=[exp(-(x-3)^2-(y-3)^2) for x in 1:5, y in 1:5].+rand(5,5)*0.3
+#
+# f1=KernelOp(M, (1,1)) do A,Is,I
+#     avg=zero(eltype(A))
+#     @inbounds for i in Is
+#         avg+=A[i]
+#     end
+#     avg/length(Is)
+# end
+#
+# f2=KernelOp(f1, (1,1)) do A,Is,I
+#     m=A[I]
+#     @inbounds for i in Is
+#         m=max(m,A[i])
+#     end
+#     m
+# end
+#
+# show(stdout,"text/plain",f1.==f2)
+#
